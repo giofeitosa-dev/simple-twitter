85ad6f802d80a6d3c5a454f4306a5f4ee3458649
Giovani <giovanifeitosa@gmail.com>
2025-10-16 18:14:11 -0300

    adding docker-compose

---
+++ backend/src/main/java/br/com/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package br.com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    
+}
